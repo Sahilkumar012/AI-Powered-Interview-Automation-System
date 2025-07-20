bbb5f8303f61d50af8bac8646ff798ddcf50439a
Sahilkumar012 <sahilkumar545657@gmail.com>
2025-07-20 19:01:18 +0500

    Add files via upload

---
+++ llama_model.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from groq import Groq
+
+filepath = "resumes_with_keywords_main.csv"
+def read_csv(file_path):
+    df = pd.read_csv(file_path)  # Read CSV into DataFrame
+    return df
+
+def generate_questions_for_rows(file_path, output_file):
+    df = read_csv(file_path)
+
+    client = Groq(api_key="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")
+
+    with open(output_file, "w", encoding="utf-8") as file:
+        for index, row in df.iterrows():
+            keywords = row['Extracted Keywords']
+
+            chat_completion = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "Analyze the keywords and generate 2 question for only two keywords not more than two."
+                    },
+                    {
+                        "role": "user",
+                        "content": keywords  # Pass the keyword of the current row
+                    }
+                ],
+                model="llama3-8b-8192",
+
+            )
+
+            questions = chat_completion.choices[0].message.content
+            file.write(f"Questions for Row {index + 1} based on keywords:\n{questions}\n\n")
+            print(f"Questions for Row {index + 1} saved.")
+
+output_file = "generated_questions_main.txt"
+generate_questions_for_rows(filepath, output_file)
+print(f"All questions have been saved in {output_file}.")
